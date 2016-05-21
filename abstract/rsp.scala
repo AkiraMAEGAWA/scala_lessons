83f519246cf07eda04e614ca90e6669e5fd637d1
arika <maeaki112@gmail.com>
2016-05-22 02:35:51 +0900

    rock_scissors_paper! 1,2,3git add rsp.scala !

--- abstract/rsp.scala
+++ abstract/rsp.scala
@@ -1,56 +1,63 @@
-//Rock Paper Scissors => rpc
+//Rock Scissors Paper => rsp
 import scala.util.Random
 
-/*
-object Rsp extends Enumeration{
-  val rock     = 1
-  val scissors = 2
-  val paper    = 3
-}
-*/
-
 val ARRAY_LENGTH = 256
-
 val numArray  = new Array[Int](ARRAY_LENGTH)
-val pc_handArray = new Array[String](ARRAY_LENGTH)
 var r = new Random
 
-def rsp_comp(YourHand: Int, pcHand: Int):String = {
-  var showResult:String = "0"
-  if(YourHand == pcHand)
-    showResult = "draw"
-	else if(YourHand % 3 == ((pcHand + 1 )% 3))
-    showResult = "Ops.... you Lose"
-	else if(((YourHand + 1 ) % 3)== pcHand % 3)
-    showResult = "You win!!!"
- //  else
-//	  showResult = "Uh-huh?"
-
-	showResult
+def toHand(numHand: Int):String = {
+  numHand match{
+	  case 1 => "rock"
+	  case 2 => "scissors"
+	  case 3 => "paper"
+	  case _ => "null"
+	}
 }
+
+def rsp_comp(YH: Int, pcHand: Int):String = {
+  var showResult:String = "0"
+  if(YH == pcHand)
+    showResult = "draw"
+	else if(YH % 3 == ((pcHand + 1 )% 3))
+    showResult = "Ops.... you Lose"
+	else if(((YH + 1 ) % 3)== pcHand % 3)
+    showResult = "You win!!!"
+ 
+  showResult
+}
+
+//begin from here↓↓↓
+for(x <- 1 to 33)
+  println("")
 
 for(i <- 0 to ARRAY_LENGTH - 1){
   numArray(i) = r.nextInt(3) + 1
-  pc_handArray(i) = 
-    numArray(i) match{
-      case 1 => "rock"
-      case 2 => "scissors"
-      case 3 => "paper"
-    }
+/* ---you can use tricks---
+ 
   if(i % 5 == 0)
-    println(pc_handArray(i))
+    println(toHand(numArray(i)))
   else
-    print(pc_handArray(i) + ": ")
+    print(toHand(numArray(i)) + ": ")
+
+*/
 }
 
 val sc = new java.util.Scanner(System.in) 
 for(i <- 0 to ARRAY_LENGTH - 1){
+  println( i + " times")
   print("rock scissors paper! 1,2,3!")
   var yourhand = sc.nextInt.toByte
-  if(yourhand == 35)
+  if(yourhand == 99){
+	  println("Good Bye")
 	  exit()
-//	println("yourhand: " + transHand(yourhand)
-  println("pcHand:  " + pc_handArray(i))
-  println(rsp_comp(yourhand, numArray(i)))
-  
+	}
+  if(toHand(yourhand) != "null"){
+    println("yourhand: " + toHand(yourhand))
+    println("pchand  : " + toHand(numArray(i))) 
+    print("--------------                                       ")
+	  println(rsp_comp(yourhand, numArray(i)))
  }
+  else
+	  println("NoGame")
+	println("******************************************************")
+}
